1d20232ff1163c8c563bc3e40d7697b34bc71f82
g3mini <g3mini@outlook.com>
2015-02-26 18:50:08 +0100

    Added files
    Added everything to github, makes things easier to manage and allows
    other people to collaborate.

---
+++ navbarUrls.js
@@ -0,0 +1,59 @@
+document.addEventListener('polymer-ready', function() {
+
+
+
+    var urlList = {
+
+        // Add navbar items here in the form of name: "what you see in the navbar", link: "The url of the page.", id: pageIdentifier"
+        urls: [{
+            name: "Home",
+            link: "",
+            id: 0
+
+        }, {
+            name: "Login",
+            link: "login",
+            id: 1
+        }, {
+            name: "Register",
+            link: "register",
+            id: 2
+        }, {
+            name: "Characters",
+            link: "characters",
+            id: 3
+        }
+
+        ]
+    };
+
+    document.querySelector("#urlTemplate").model = urlList;
+
+
+
+
+    // On page load render the correct page.
+
+    // Url = #page
+    var url = window.location.hash;
+    //Url becomes page (# is removed.)
+    url = url.substr(1);
+    // ForEach page in array urlList
+    urlList['urls'].forEach(function(urls) {
+        // Check if the url is the same as the variable url (without the #)
+        if (urls['link'] === url) {
+            // Set current page to #page
+           navigate(urls['link'],urls['name'],urls['id'])
+        }
+    });
+
+});
+
+
+function navigate(url, name, id) {
+    window.location.hash = url;
+    document.querySelector("#topText").innerHTML = name;
+    var p = document.querySelector('#navPages');
+    p.selected = id;
+
+}
